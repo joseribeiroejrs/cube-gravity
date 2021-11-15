ab394b8dfc28fef39f9604eef332a4704c520b2d
Eduardo Soares <eduardodeiturama@hotmail.com>
2021-11-15 20:21:55 -0300

    Adicionando menu de seleção de levels

--- Assets/Scripts/Menu/MenuScript.cs
+++ Assets/Scripts/Menu/MenuScript.cs
@@ -7,9 +7,11 @@ public class MenuScript : MonoBehaviour
 {
 	public GameObject arcadeLevels;
 	public GameObject menuPrincipal;
+	public GameObject selectMode;
 	public void PlayEvent()
 	{
-		arcadeLevels.SetActive(true);
+		arcadeLevels.SetActive(false);
+		selectMode.SetActive(true);
 		menuPrincipal.SetActive(false);
 	}
 
@@ -17,8 +19,21 @@ public class MenuScript : MonoBehaviour
 	{
 		arcadeLevels.SetActive(false);
 		menuPrincipal.SetActive(true);
+		selectMode.SetActive(false);
 	}
 
+	public void PlayInfinityMode()
+	{
+		int INFINITY_LEVEL = 21;
+		SceneManager.LoadScene(INFINITY_LEVEL);
+	}
+
+	public void PlayArcadeMode()
+	{
+		arcadeLevels.SetActive(true);
+		selectMode.SetActive(false);
+		menuPrincipal.SetActive(false);
+	}
 	public void QuitEvent() 
 	{
 		Application.Quit();
